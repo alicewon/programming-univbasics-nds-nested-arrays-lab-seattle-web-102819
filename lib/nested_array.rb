cc39333fdfecad1ab96051066b176349ec80b984
Alice Won <alicewon4@gmail.com>
2019-10-25 02:36:10 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -19,10 +19,8 @@ ENFIELD_TENNIS_ACADEMY = [
 def assembled_matrix
   # Build an array that contains (or, "nests") the residents of The Ennet House
   # and the Enfield Tennis Academy as provided by the constants
-  assembled_array = [
-    ["Don Gately", "Joelle van Dyne", "Pat M.", "Kate Gompert","Bruce Green"],
-    ["Hal Incandenza", "Lyle", "Gerhard Schtitt", "Mario Incandenza", "Michael Pemulis"]
-  ]
+  assembled_array = [["Don Gately", "Joelle van Dyne", "Pat M.", "Kate Gompert","Bruce Green"],
+    ["Hal Incandenza", "Lyle", "Gerhard Schtitt", "Mario Incandenza", "Michael Pemulis"]]
 end
 
 def array_literal_matrix
